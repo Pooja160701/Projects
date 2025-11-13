9347991952673081928f81189f8fb1162cf899e1
Pooja160701 <poojaajithan160701@gmail.com>
2025-11-13 16:43:48 +0530

    Deletion of Project

--- sql-etl-netflix/models/marts/dim_countries.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    country,
-    COUNT(show_id) AS total_titles,
-    COUNT(CASE WHEN type = 'movie' THEN 1 END) AS total_movies,
-    COUNT(CASE WHEN type = 'tv show' THEN 1 END) AS total_shows
-FROM {{ ref('stg_netflix_clean') }}
-GROUP BY country
-ORDER BY total_titles DESC
